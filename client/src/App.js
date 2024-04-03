88d922069530048ac767e6e5cb1168d64324c3b9
Erik Centeno <erikcenteno@proton.me>
2024-04-02 22:26:33 -0400

    Added branding basics to site!

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to the books app!</h1>
+        <h1>NOVEL CAT</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Every book has a story.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Download the App
         </a>
       </header>
     </div>
